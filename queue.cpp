792856b4f73a584df8e868e27ce6da67f60f9793
Siti Rafeyfa Asyla. P <2510817220018@mhs.ulm.ac.id>
2026-04-21 14:51:41 +0800

    selesai mengerjakan queue

--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,47 @@
 #include "queue.h"
+#include <stdexcept>
+
+void init(Queue* q) {
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+bool isEmpty(const Queue* q) {
+    return (q->front == NULL);
+}
+
+bool isFull(const Queue* q) {
+    if (q->rear == &(q->data[MAX - 1])) return true;
+    return false;
+}
+
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) throw std::runtime_error("Queue Penuh!");
+    
+    if (isEmpty(q)) {
+        q->front = &(q->data[0]);
+        q->rear = &(q->data[0]);
+    } else {
+        q->rear++;
+    }
+    *(q->rear) = value;
+}
+
+void dequeue(Queue* q) {
+    if (isEmpty(q)) throw std::runtime_error("Queue Kosong!");                
+    
+    if (q->front == q->rear) {
+        q->front = NULL;
+        q->rear = NULL;
+    } else {
+        q->front++;
+    }
+}
+
+int front(const Queue* q) {
+    return *(q->front);
+}
+
+int back(const Queue* q) {
+    return *(q->rear);
+}
