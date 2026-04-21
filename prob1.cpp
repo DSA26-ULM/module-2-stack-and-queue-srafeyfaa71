d1c376c44e9767e9b3ec23877bff3dafead3beaa
Siti Rafeyfa Asyla. P <2510817220018@mhs.ulm.ac.id>
2026-04-21 14:51:05 +0800

    selesai mengerjakan prob1

--- prob1.cpp
+++ prob1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
 #include "stack.h"
+
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    Stack s;
+    init(&s);
+    string input;
+
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        if (input == "+" || input == "-" || input == "*" || input == "/") {
+            int bil2 = peek(&s); pop(&s);
+            int bil1 = peek(&s); pop(&s);
+
+            if (input == "+") push(&s, bil1 + bil2);
+            else if (input == "-") push(&s, bil1 - bil2);
+            else if (input == "*") push(&s, bil1 * bil2);
+            else if (input == "/") push(&s, bil1 / bil2);
+        } else {
+            push(&s, stoi(input));
+        }
+    }
+    cout << peek(&s) << endl;
+    return 0;
+}
