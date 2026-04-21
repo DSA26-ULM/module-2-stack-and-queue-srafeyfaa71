2ea280e7e0c1735453d3c9114c4018bed5e87708
Siti Rafeyfa Asyla. P <2510817220018@mhs.ulm.ac.id>
2026-04-21 14:51:54 +0800

    selesai mengerjakan stack

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,36 @@
 #include "stack.h"
+#include <stdexcept>
 
+void init(Stack* s) {
+    s->top = &(s->data[-1]); 
+}
+
+bool isEmpty(const Stack* s) {
+    return (s->top == &(s->data[-1]));
+}
+
+bool isFull(const Stack* s) {
+    return (s->top == &(s->data[MAX - 1]));
+}
+
+void push(Stack* s, int value) {
+    if (isFull(s)) {
+        throw std::runtime_error("Stack Penuh!");
+    }
+    s->top++;
+    *(s->top) = value;
+}
+
+void pop(Stack* s) {
+    if (isEmpty(s)) {
+        throw std::runtime_error("Stack Kosong!");
+    }
+    s->top--;
+}
+
+int peek(const Stack* s) {
+    if (isEmpty(s)) {
+        throw std::runtime_error("Stack Kosong!");
+    }
+    return *(s->top);
+}
